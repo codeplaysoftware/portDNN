26b6770524db6fd1f50587ffc8a00e85eb51ea06
John Lawson <john@codeplay.com>
2019-03-15 14:52:06 +0000

    Add tile sizes for AMD and Intel GPUs

--- src/conv2d/tiled/launch_tiled.cc
+++ src/conv2d/tiled/launch_tiled.cc
@@ -179,6 +179,25 @@ inline SNNStatus launch_tiled(ReadAccessor<T const> input,
   LAUNCH_IF_MATCH(params, 1, 1, 3, 4, 4, 1)
   LAUNCH_IF_MATCH(params, 1, 1, 2, 4, 1, 1)
 #endif
+#ifdef AMD_GPU
+  LAUNCH_IF_MATCH(params, 3, 1, 4, 5, 4, 2)
+  LAUNCH_IF_MATCH(params, 3, 1, 4, 5, 2, 2)
+  LAUNCH_IF_MATCH(params, 3, 1, 5, 5, 4, 1)
+  LAUNCH_IF_MATCH(params, 3, 1, 4, 3, 1, 4)
+  LAUNCH_IF_MATCH(params, 3, 1, 5, 4, 1, 1)
+  LAUNCH_IF_MATCH(params, 1, 1, 1, 5, 4, 8)
+  LAUNCH_IF_MATCH(params, 1, 1, 2, 3, 4, 4)
+  LAUNCH_IF_MATCH(params, 1, 1, 2, 5, 1, 8)
+  LAUNCH_IF_MATCH(params, 1, 1, 3, 4, 8, 1)
+  LAUNCH_IF_MATCH(params, 1, 1, 2, 3, 1, 1)
+#endif
+#ifdef INTEL_GPU
+  LAUNCH_IF_MATCH(params, 3, 1, 3, 3, 1, 4)
+  LAUNCH_IF_MATCH(params, 3, 1, 5, 4, 1, 1)
+  LAUNCH_IF_MATCH(params, 1, 1, 4, 2, 4, 8)
+  LAUNCH_IF_MATCH(params, 1, 1, 3, 4, 1, 8)
+  LAUNCH_IF_MATCH(params, 1, 1, 3, 4, 1, 1)
+#endif
   if(std::is_same<ConvType, conv_type::Forward>::value) {
     LAUNCH_IF_MATCH(params, 1, 2, 1, 2, 1, 4)
     LAUNCH_IF_MATCH(params, 1, 2, 1, 2, 1, 1)
