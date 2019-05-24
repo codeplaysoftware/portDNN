12a3f07aa242e439de0bca3fe31afc93a352c92d
John Lawson <john@codeplay.com>
2019-05-24 12:14:36 +0100

    Add clang-format checks for template files

--- hooks/clang-format-all.sh
+++ hooks/clang-format-all.sh
@@ -5,5 +5,5 @@ IFS=$'\n\t'
 
 VERSION=${VERSION:-""}
 
-git ls-files | grep -E "*\.h$|*\.hpp$|*\.cc$|*\.cpp$" | \
+git ls-files | grep -E "*\.h$|*\.hpp$|*\.cc$|*\.cpp$|*\.cc.in$" | \
   xargs --max-procs=`nproc` --max-args=1 clang-format$VERSION -style=file -i
