85ebef2ff7ff6c8ca91da5469c10484187c17d5c
John Lawson <john@codeplay.com>
2019-06-25 10:42:11 +0100

    Provide single SYCL-BLAS backend
    Rather than constructing a new backend for each backend provider, share
    a single backend. This prevents the construction of multiple (identical)
    SYCL objects at initialisation, which makes OpenCL traces almost
    impossible to read as the first thousand calls are clGetDeviceInfo and
    clGetPlatformInfo.

--- src/backend/syclblas_backend_provider.h
+++ src/backend/syclblas_backend_provider.h
@@ -21,6 +21,10 @@
 
 #include "src/backend/backend_provider.h"
 
+#include <stdexcept>
+
+#include <CL/sycl.hpp>
+
 namespace sycldnn {
 namespace backend {
 /**
@@ -31,19 +35,23 @@ namespace backend {
  */
 template <>
 struct BackendProvider<SyclBLASBackend> {
-  BackendProvider() : backend_{get_default_queue()} {}
-  SNN_DISABLE_COPY(BackendProvider);
-
   template <typename T>
   using Pointer = SyclBLASBackend::pointer_type<T>;
 
-  /** Return this backend. */
-  SyclBLASBackend& get_backend() { return backend_; }
+  /** Return a reference to the SYCL-BLAS backend. */
+  SyclBLASBackend& get_backend() {
+    // Keep a single SYCL-BLAS backend for the duration of program execution to
+    // limit the number of SYCL contexts and devices that need to be
+    // constructed.
+    static SyclBLASBackend backend{get_default_queue()};
+    return backend;
+  }
+
   /** Allocate memory on the device and initialise it with the provided data. */
   template <typename T>
   Pointer<T> get_initialised_device_memory(size_t size,
                                            std::vector<T> const& data) {
-    auto executor = backend_.get_executor().get_policy_handler();
+    auto executor = get_backend().get_executor().get_policy_handler();
     auto gpu_ptr = executor.template allocate<T>(size);
     if (size != 0u) {
       try {
@@ -63,7 +71,7 @@ struct BackendProvider<SyclBLASBackend> {
   void copy_device_data_to_host(size_t size, Pointer<T> gpu_ptr,
                                 std::vector<T>& host_data) {
     host_data.resize(size);
-    auto executor = backend_.get_executor().get_policy_handler();
+    auto executor = get_backend().get_executor().get_policy_handler();
     if (size != 0u) {
       auto event_list = executor.copy_to_host(gpu_ptr, host_data.data(), size);
       for (auto& event : event_list) {
@@ -74,22 +82,20 @@ struct BackendProvider<SyclBLASBackend> {
   /** Deallocate a device pointer. */
   template <typename T>
   void deallocate_ptr(Pointer<T> ptr) {
-    backend_.get_executor().get_policy_handler().deallocate(ptr);
+    get_backend().get_executor().get_policy_handler().deallocate(ptr);
   }
 
   /**
    * Keep a static instance of an cl::sycl::queue to prevent rebuilding
    * kernels for every benchmark instance.
    */
-  cl::sycl::queue get_default_queue() {
+  cl::sycl::queue& get_default_queue() {
     auto exception_handler = [](cl::sycl::exception_list exceptions) {
       for (std::exception_ptr const& e : exceptions) {
         try {
           std::rethrow_exception(e);
         } catch (cl::sycl::exception const& e) {
-          std::cerr << "Caught asynchronous SYCL exception:\n"
-                    << e.what() << std::endl;
-          throw;
+          throw std::runtime_error(e.what());
         }
       }
     };
@@ -97,9 +103,6 @@ struct BackendProvider<SyclBLASBackend> {
                                  exception_handler};
     return queue;
   }
-
- private:
-  SyclBLASBackend backend_;
 };
 
 }  // namespace backend
