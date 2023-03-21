838e2025bd095b4331d103795f5f8809dbc5501b
Aidan Belton <aidan.belton@codeplay.com>
2023-03-21 12:47:57 +0000

    Update the default selector test for USM

--- test/conv2d/default_selector.cc
+++ test/conv2d/default_selector.cc
@@ -20,55 +20,60 @@
 #include "sycldnn/conv2d/sizes.h"
 
 #include "src/backend/snn_backend_provider.h"
+#include "src/backend/snn_usm_backend_provider.h"
 #include "sycldnn/backend/snn_backend.h"
+#include "sycldnn/backend/snn_usm_backend.h"
+#include "test/backend/backend_test_fixture.h"
+
+#include "test/types/test_backend_types.h"
+#include "test/types/to_gtest_types.h"
 
 #include <memory>
 
 #include <CL/sycl.hpp>
 
-TEST(DefaultSelectorTest, GetReturnsNonNull) {
-  // Construct a SYCL queue with the default selector.
-  cl::sycl::queue q;
+template <typename Backend>
+struct DefaultSelectorFixture : public BackendTestFixture<Backend> {
+ protected:
+  void check_conv_launch_successful(
+      sycldnn::conv2d::Conv2DParams const& params) {
+    using ConvType = sycldnn::conv2d::conv_type::Forward;
 
-  // Use the queue to get a device reference, then validate that we return a
-  // non-null selector.
-  auto selector = sycldnn::conv2d::get_default_selector(q.get_device());
-  EXPECT_TRUE(nullptr != selector);
-}
+    using HostData = std::vector<float>;
 
-namespace {
-void check_conv_launch_successful(sycldnn::conv2d::Conv2DParams const& params) {
-  using Backend = sycldnn::backend::SNNBackend;
-  using BackendProvider = sycldnn::backend::BackendProvider<Backend>;
-  using ConvType = sycldnn::conv2d::conv_type::Forward;
+    auto& provider = this->provider_;
+    auto device = provider.get_backend().get_queue().get_device();
 
-  using HostData = std::vector<float>;
+    auto sizes = sycldnn::conv2d::get_sizes<ConvType>(params);
+    HostData input(sizes.input_size);
+    HostData filter(sizes.filter_size);
+    HostData output(sizes.output_size);
 
-  BackendProvider provider;
-  auto device = provider.get_backend().get_queue().get_device();
+    auto input_gpu =
+        provider.get_initialised_device_memory(sizes.input_size, input);
+    auto filter_gpu =
+        provider.get_initialised_device_memory(sizes.filter_size, filter);
+    auto output_gpu =
+        provider.get_initialised_device_memory(sizes.output_size, output);
 
-  auto sizes = sycldnn::conv2d::get_sizes<ConvType>(params);
-  HostData input(sizes.input_size);
-  HostData filter(sizes.filter_size);
-  HostData output(sizes.output_size);
+    // Use the queue to get a device reference, then validate that we return a
+    // non-null selector.
+    auto selector = sycldnn::conv2d::get_default_selector(device);
+    EXPECT_TRUE(nullptr != selector);
+    auto status = sycldnn::conv2d::launch<float, ConvType>(
+        input_gpu, filter_gpu, output_gpu, params, *selector,
+        provider.get_backend());
+    ASSERT_EQ(sycldnn::StatusCode::OK, status.status);
+    status.event.wait_and_throw();
+  }
+};
 
-  auto input_gpu =
-      provider.get_initialised_device_memory(sizes.input_size, input);
-  auto filter_gpu =
-      provider.get_initialised_device_memory(sizes.filter_size, filter);
-  auto output_gpu =
-      provider.get_initialised_device_memory(sizes.output_size, output);
+template <typename Backend>
+using DefaultSelectorTest = DefaultSelectorFixture<Backend>;
 
-  auto selector = sycldnn::conv2d::get_default_selector(device);
-  auto status = sycldnn::conv2d::launch<float, ConvType>(
-      input_gpu, filter_gpu, output_gpu, params, *selector,
-      provider.get_backend());
-  ASSERT_EQ(sycldnn::StatusCode::OK, status.status);
-  status.event.wait_and_throw();
-}
-}  // namespace
+TYPED_TEST_SUITE(DefaultSelectorTest, sycldnn::types::GTestDefaultBackendTypes);
 
-TEST(DefaultSelectorTest, GetValidSelectionFor5x5s2) {
+TYPED_TEST(DefaultSelectorTest, GetValidSelectionFor5x5s2) {
   sycldnn::conv2d::Conv2DParams params;
   params.channels = 3;
   params.features = 32;
@@ -85,5 +90,5 @@ TEST(DefaultSelectorTest, GetValidSelectionFor5x5s2) {
   params.pad_cols = 1;
   params.dilation_rows = 1;
   params.dilation_cols = 1;
-  check_conv_launch_successful(params);
+  this->check_conv_launch_successful(params);
 }
