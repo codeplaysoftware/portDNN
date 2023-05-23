1a916948c3d832d4805554162d5bf1621b125cab
Joe Todd <joe.todd@codeplay.com>
2023-05-23 19:35:01 +0100

    Adding NCHW tiled conv2d to build system

--- src/conv2d/tiled/launch_tiled.cc
+++ src/conv2d/tiled/launch_tiled.cc
@@ -323,15 +323,13 @@ inline SNNStatus launch_tiled_impl(MemObj<T const>& input,
 
   } else { //NCHW
 
-// TODO InputBackprop tiled!
-
     // clang-format off
-    // LAUNCH_IF_MATCH(params, 1, 2, 2, 2, 1, 1)
-    // LAUNCH_IF_MATCH(params, 3, 1, 3, 4, 1, 1)
-    // LAUNCH_IF_MATCH(params, 3, 2, 2, 2, 1, 1)
-    // LAUNCH_IF_MATCH(params, 5, 1, 2, 4, 1, 1)
-    // LAUNCH_IF_MATCH(params, 1, 1, 2, 2, 1, 1)
-    // LAUNCH_IF_MATCH(params, 1, 2, 2, 2, 1, 1)
+    LAUNCH_IF_MATCH(params, 1, 2, 2, 2, 1, 1)
+    LAUNCH_IF_MATCH(params, 3, 1, 3, 4, 1, 1)
+    LAUNCH_IF_MATCH(params, 3, 2, 2, 2, 1, 1)
+    LAUNCH_IF_MATCH(params, 5, 1, 2, 4, 1, 1)
+    LAUNCH_IF_MATCH(params, 1, 1, 2, 2, 1, 1)
+    LAUNCH_IF_MATCH(params, 1, 2, 2, 2, 1, 1)
 
     return StatusCode::InvalidAlgorithm;
 
